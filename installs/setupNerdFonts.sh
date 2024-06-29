0e3cf8dbc77caef1cb75471770ddfb294add3ca4
Jerzy Stusinski <tojastuch@gmail.com>
2024-06-29 10:57:34 +0200

    fuller conf, soon going to stow

--- installs/setupNerdFonts.sh
+++ installs/setupNerdFonts.sh
@@ -1,12 +1,6 @@
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CommitMono.zip
-
-
 mkdir ~/.fonts
-
 unzip CommitMono.zip -d ~/.fonts
-
 rm CommitMono.zip
-
 fc-cache -fv
-
 echo "gnome-terminal; go to preferences and change font to CommitMono"
