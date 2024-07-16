462741c3f1323421d9d9d7bc2a1b89f426a36775
Jerzy Stusinski <tojastuch@gmail.com>
2024-07-16 08:52:00 +0200

    zsh aliases to dat and stuff

--- install/installZsh.sh
+++ install/installZsh.sh
@@ -11,18 +11,18 @@ sed -i '/ZSH_THEME/d' ~/.zshrc
 
 exit 0
 
-#echo '. "$HOME/.cargo/env"' >> ~/.zshrc
-#echo 'alias ls="eza --icons"' >> ~/.zshrc
-#echo 'alias ccat="cat"' >> ~/.zshrc
-#echo 'alias bat="bat"' >> ~/.zshrc
-#echo 'alias dat="bat -p"' >> ~/.zshrc
-#echo 'export VISUAL=vim' >> ~/.zshrc
-#echo 'export EDITOR="$VISUAL"' >> ~/.zshrc
+echo '. "$HOME/.cargo/env"' >> ~/.zshrc
+echo 'alias ls="eza --icons"' >> ~/.zshrc
+echo 'alias ccat="cat"' >> ~/.zshrc
+echo 'alias bat="bat"' >> ~/.zshrc
+echo 'alias dat="bat -p"' >> ~/.zshrc
+echo 'export VISUAL=vim' >> ~/.zshrc
+echo 'export EDITOR="$VISUAL"' >> ~/.zshrc
 #echo 'export PICO_SDK_PATH=~/pico/pico-sdk' >> ~/.zshrc
 #echo 'export PICO_EXAMPLES_PATH=~/pico/pico-examples' >> ~/.zshrc
 #echo 'export PICO_SDK_PATH=~/pico/pico-sdk' >> ~/.zshrc
 #echo 'export PICO_EXAMPLES_PATH=~/pico/pico-examples' >> ~/.zshrc
 #echo 'figlet -c `fortune` | lolcat' >> ~/.zshrc
 #echo 'alias get_idf=". /home/jur/esp/esp-idf/export.sh"' >> ~/.zshrc
-#echo 'source /usr/share/doc/fzf/examples/key-bindings.zsh' >> ~/.zshrc
-#echo 'export HISTSIZE=1000000000' >> ~/.zshrc
+echo 'source /usr/share/doc/fzf/examples/key-bindings.zsh' >> ~/.zshrc
+echo 'export HISTSIZE=1000000000' >> ~/.zshrc
