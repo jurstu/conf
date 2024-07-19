10351135deeb34d8970960a8f7d60c1bbe5ffa23
Jerzy Stusinski <tojastuch@gmail.com>
2024-07-19 12:12:27 +0200

    pymavlink and zsh aliases

---
+++ install/pyMavlink.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt install python3 python3-pip
+
+sudo apt-get install libxml2-dev libxslt-dev
+
+sudo apt-get install python3-numpy python3-pytest
+
+sudo python -m pip install --upgrade future lxml
+
+
